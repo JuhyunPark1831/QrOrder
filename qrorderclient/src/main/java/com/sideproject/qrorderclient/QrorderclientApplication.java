a1a17c35d9fb1c627dee8aaf1210d3f3c788aee7
JuhyunPark <epahs1831@naver.com>
2025-03-26 16:30:02 +0900

    프로젝트 환경 세팅 -#1

---
+++ qrorderclient/src/main/java/com/sideproject/qrorderclient/QrorderclientApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.qrorderclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrorderclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrorderclientApplication.class, args);
+	}
+
+}
---
+++ qrordermanager/src/main/java/com/sideproject/qrordermanager/QrordermanagerApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.qrordermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrordermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrordermanagerApplication.class, args);
+	}
+
+}
