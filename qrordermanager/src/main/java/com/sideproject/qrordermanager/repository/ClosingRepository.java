c857561fa12ce72845e5246db7bbfcc375efb334
JuhyunPark <epahs1831@naver.com>
2025-03-29 00:13:54 +0900

    Feature: (manager) JPA Repository 작성 -#7

---
+++ qrordermanager/src/main/java/com/sideproject/qrordermanager/repository/ClosingRepository.java
@@ -0,0 +1,7 @@
+package com.sideproject.qrordermanager.repository;
+
+import com.sideproject.qrordermanager.entity.Closing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClosingRepository extends JpaRepository<Long, Closing> {
+}
---
+++ qrordermanager/src/main/java/com/sideproject/qrordermanager/repository/MenuRepository.java
@@ -0,0 +1,7 @@
+package com.sideproject.qrordermanager.repository;
+
+import com.sideproject.qrordermanager.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Long, Menu> {
+}
