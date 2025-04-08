0ec0919463c36aa435b54502354e3a373330f8fc
JuhyunPark <epahs1831@naver.com>
2025-04-08 15:48:25 +0900

    Feature: (manager) 메뉴 관리 관련 페이지 개발 -#18

--- qrordermanager/src/main/java/com/sideproject/qrordermanager/entity/Common/ENUM/MenuStatus.java
+++ qrordermanager/src/main/java/com/sideproject/qrordermanager/entity/Common/ENUM/MenuStatus.java
@@ -2,6 +2,7 @@ package com.sideproject.qrordermanager.entity.Common.ENUM;
 
 public enum MenuStatus {
     AVAILABLE,        // 판매중
+    HIDDEN,
     TEMPORARILY_OUT,  // 일시 품절
     PERMANENTLY_OUT   // 반영구 품절
 }
