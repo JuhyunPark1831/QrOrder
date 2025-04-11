c7b43a890d8fefe382a50ef2688eed84b26a6e88
JuhyunPark <epahs1831@naver.com>
2025-04-11 17:17:51 +0900

    Feature: (manager) 품절 관리 페이지 개발 -#29

---
+++ qrordermanager/src/main/java/com/sideproject/qrordermanager/repository/MenuSoldOutRepository.java
@@ -0,0 +1,7 @@
+package com.sideproject.qrordermanager.repository;
+
+import com.sideproject.qrordermanager.entity.MenuSoldOut;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuSoldOutRepository extends JpaRepository<MenuSoldOut, Long> {
+}
