dd491ef6081348cbcd7ed4d81f2ff315428af821
JuhyunPark <epahs1831@naver.com>
2025-05-16 14:47:33 +0900

    Feature: (client) 고객 메뉴 페이지 메뉴 숨김 처리 -#90

--- src/main/java/com/sideProject/qrOrder/controller/client/ClientController.java
+++ src/main/java/com/sideProject/qrOrder/controller/client/ClientController.java
@@ -1,5 +1,7 @@
 package com.sideProject.qrOrder.controller.client;
 
+import com.sideProject.qrOrder.dto.menu.MenuDto;
+import com.sideProject.qrOrder.entity.Common.ENUM.MenuStatus;
 import com.sideProject.qrOrder.service.category.CategoryService;
 import com.sideProject.qrOrder.service.menu.MenuService;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +25,9 @@ public class ClientController {
 
         model.addAttribute("categoryList", categoryService.selectCategory(Pageable.unpaged(), null));
 
-        model.addAttribute("menuList", menuService.selectMenu(Pageable.unpaged(), null));
+        model.addAttribute("menuList", menuService.selectMenu(Pageable.unpaged(), MenuDto.builder()
+                .meStatus(MenuStatus.HIDDEN)
+                .build()));
 
         model.addAttribute("menuDetail", null);
 
@@ -44,3 +48,5 @@ public class ClientController {
         return "/client/pages/orderList";
     }
 }
+
+//todo: 메뉴 관리 페이지 메뉴 상태 체크
