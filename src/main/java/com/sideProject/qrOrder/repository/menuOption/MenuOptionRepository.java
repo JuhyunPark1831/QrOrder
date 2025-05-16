f615f5c2c6d2dcfc3d634820cf77021b19890be0
JuhyunPark <epahs1831@naver.com>
2025-05-16 17:38:08 +0900

    Update: (manager) 메뉴 옵션 품절 시 최소 선택 개수와 비교 로직 작성중 2025-05-16 -#98

--- src/main/java/com/sideProject/qrOrder/repository/menuOption/MenuOptionRepository.java
+++ src/main/java/com/sideProject/qrOrder/repository/menuOption/MenuOptionRepository.java
@@ -1,5 +1,6 @@
 package com.sideProject.qrOrder.repository.menuOption;
 
+import com.sideProject.qrOrder.entity.Common.ENUM.SoldOutStatus;
 import com.sideProject.qrOrder.entity.Menu;
 import com.sideProject.qrOrder.entity.MenuOption;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,6 @@ import java.util.List;
 public interface MenuOptionRepository extends JpaRepository<MenuOption, Long>, MenuOptionCustomRepository {
 
     public List<MenuOption> findByOpOg_OgIdIn(List<Long> ogIds);
+    long countByOpOg_OgId(Long ogId);
     public void deleteByOpOg_OgId(Long ogId);
 }
